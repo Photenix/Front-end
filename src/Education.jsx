584af9ca3d1bb59bd5befacb7fbb73e66d566edf
Photenix <juanmanuelpinor@gmail.com>
2023-09-09 13:12:28 -0500

    education

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,9 +1,9 @@
 const Education = () => {
     return (
-        <div>
-            <h2>Educación</h2>
+        <div className="education">
+            <h2 className="font-medium">Educación</h2>
             <div>
-                <h3>Aprende ahora</h3>
+                <h3 className="font-medium">Aprende ahora</h3>
                 <p>Aprende a ahorrar y manejar mejor tus finanzas</p>
             </div>
 
@@ -11,7 +11,7 @@ const Education = () => {
                 <div className="top_education">
                     <img src="" alt="" />
                     <div>
-                        <h3>title</h3>
+                        <h3 className="font-medium">title</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem, dolore aut alias fugiat vero expedita doloremque. Hic, quasi laudantium vitae, dolor placeat molestiae incidunt officiis ipsum nostrum odit sapiente.</p>
                     </div>
                 </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 // import Index from './Index.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Calculator from './assets/calculator/Calculator';
+import Education from './Education';
 
 
 const router = createBrowserRouter([
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <Calculator />,
   },
+  {
+    path: "/educacion",
+    element: <Education/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
