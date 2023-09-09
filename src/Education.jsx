155d86ded9c3b818a9b37950e39a1eaa279ad701
Photenix <juanmanuelpinor@gmail.com>
2023-09-09 12:02:16 -0500

    education

---
+++ src/Education.jsx
@@ -0,0 +1,12 @@
+const Education = () => {
+    return (
+        <div>
+            <h2>Educacion</h2>
+            <div>
+                info
+            </div>
+        </div>
+    )
+}
+  
+export default Education
