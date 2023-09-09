cb09de839cc91b97f84d1e9be543a8c60f23ac44
nuthelk <nuthelkcd@gmail.com>
2023-09-09 12:07:51 -0500

    add background color

---
+++ src/assets/calculator/Calculator.jsx
@@ -0,0 +1,13 @@
+
+
+const Calculator = () => {
+  return (
+    <dive className="w-screen ">
+        <h1>Calculator</h1>
+        <button className="rounded-lg p-6 bg-fourty text-white font-medium">get ready</button>
+        <input type="text" />
+    </dive>
+  )
+}
+
+export default Calculator 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Index from './Index.jsx'
+// import Index from './Index.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Calculator from './assets/calculator/Calculator';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Index />,
+    element: <Calculator />,
   },
 ]);
 
